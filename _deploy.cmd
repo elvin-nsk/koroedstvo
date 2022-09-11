97d5516844ac6721bdf2197eee6de205ad0a0b7f
elvin-nsk <me@elvin.nsk.ru>
2022-09-11 20:23:50 +0700

    automatic commit

--- _deploy.cmd
+++ _deploy.cmd
@@ -1,6 +1,11 @@
 call _build
+git add .
+git commit -m "automatic commit"
+git push https://elvin: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@github.com/elvin-nsk/koroedstvo.git
+
 cd _site
 git add .
 git commit -m "automatic commit"
-git push
+git push https://elvin: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@github.com/elvin-nsk/koroedstvo.git
+
 pause
