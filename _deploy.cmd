5a2f0d857904546fc8da9b83bf824404e4707591
elvin-nsk <me@elvin.nsk.ru>
2022-11-23 13:41:25 +0700

    automatic commit

--- _deploy.cmd
+++ _deploy.cmd
@@ -3,11 +3,11 @@ call _build
 cd _site
 git add .
 git commit -m "automatic commit"
-call %~dp0../push_with_auth.cmd
+call %~dp0../push_with_auth.secret
 
 cd ..
 git add .
 git commit -m "automatic commit"
-call %~dp0../push_with_auth.cmd
+call %~dp0../push_with_auth.secret
 
 pause
